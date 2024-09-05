fd05e9303e3b10a4ddaf61c4e4ae34657653ba68
shalr-20 <shalini.roy2035@gmail.com>
2024-09-05 18:32:29 +0530

    optional_nullable_vars

---
+++ optional_nullable_vars.dart
@@ -0,0 +1,7 @@
+void main() {
+  // String name = null; (not a correct way to assign null value)
+  String? name; //can also remove '= null'
+  print(name);
+  name = 'Hello World';
+  print(name);
+}
