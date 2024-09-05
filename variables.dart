e41138814a5ae79850d8111105c89c53ba22d17f
shalr-20 <shalini.roy2035@gmail.com>
2024-09-05 13:23:26 +0530

    variables

--- variables.dart
+++ variables.dart
@@ -1,19 +1,7 @@
 void main() {
-  var a = '10';
-  final b = '10';
+  final b = DateTime.now();
   const c = '10';
 
-  print(a);
-  print(b);
-  print(c);
-
-  print('--------------');
-
-  a = '1001';
-  // b = '1001'; (final value) - immutable
-  // c = '1001'; (const value) - immutable
-
-  print(a);
   print(b);
   print(c);
 }
