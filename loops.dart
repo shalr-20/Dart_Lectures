43a4c2ee50403324d7ab8917479b471f1ce688ec
shalr-20 <shalini.roy2035@gmail.com>
2024-09-06 14:44:10 +0530

    loops

--- loops.dart
+++ loops.dart
@@ -1,7 +1,22 @@
 void main() {
   //for loop
-
-  for (int i = 0; i <= 3; i++) { 
-    print("hello");
+  String val = 'hello';
+  for (int i = 0; i < val.length; i++) {
+    print(val[i]);
   }
+
+  //while loop
+  int i = 0;
+  while (i < val.length) {
+    print(val[i]);
+    i++;
+  }
+
+  //do-while loop
+  int i = 0;
+
+  do {
+    print(val[i]);
+    i++;
+  } while (i < val.length);
 }
