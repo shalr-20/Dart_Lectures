6f064c191fe81e86484da519f03a43eefa1b876e
shalr-20 <shalini.roy2035@gmail.com>
2024-09-06 22:09:32 +0530

    functions

--- functions.dart
+++ functions.dart
@@ -4,8 +4,13 @@ void main() {
     ...
   }
   */
-  print(printName().$1); //calling a function
-}
+
+//   var (age, name, isAdult, text) = printName();
+//   print(age); //calling a function
+//   print(name);
+//   print(isAdult);
+//   print(text);
+// }
 
 // String printname() {
 //   //declaring a function
@@ -16,6 +21,35 @@ void main() {
 //   return 1;
 // }
 
-(int, String) printName() {
-  return (12, 'Helllo');
+// (int, String, bool, String) printName() { //(positional arguments)
+//   return (20, 'Aditi', true, 'Hi');
+
+//   String name = 'Kumar';
+//   printName(name);
+//   print(name);
+// }
+
+// void printName(String name1) {
+//   name1 = 'Sahil';
+//   print(name1);
+
+  // printName(20, true, name: 'Sahil', greeting: 'hiee');
+
+  final stuff = printStuff();
+  print(stuff.age);
+  print(stuff.name);
+}
+
+// void printName(
+//     {required String name, int? age, required String greeting}) { //(named arguments)
+//   print(age);
+// }
+
+// void printName(int age, bool isAdult, {required name, required greeting}) {
+//   //(positional and named arguments)
+//   print(name);
+// }
+
+({int age, String name}) printStuff() {
+  return (age: 12, name: 'Sahil');
 }
