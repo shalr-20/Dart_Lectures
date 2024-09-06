fefec26a3647e72fc2fea3e868a01898fe2d6295
shalr-20 <shalini.roy2035@gmail.com>
2024-09-06 11:49:25 +0530

    control_flow

---
+++ control_flow.dart
@@ -0,0 +1,16 @@
+void main() {
+  //If, else if, else statements
+  // int age = 25;
+  // if (age >= 18 && age < 21) {
+  //   print("adult");
+  // } else if (age >= 21) {
+  //   print("adule 21");
+  // } else {
+  //   print("child");
+  // }
+
+  //Ternary operator
+  String val = "Hi";
+  String v = val.startsWith('H') ? "Wow" : "naha";
+  print(v);
+}
