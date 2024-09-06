c5d3124984bbda0fef9cb17a74837ae3b62da189
shalr-20 <shalini.roy2035@gmail.com>
2024-09-06 18:56:20 +0530

    break_continue

--- break_continue.dart
+++ break_continue.dart
@@ -1,3 +1,19 @@
-void main(){
-  
+void main() {
+  String val = 'Hello';
+
+  //continue
+  for (int i = 0; i < val.length; i++) {
+    if (i == 1 || i == 2 || i == 3) {
+      continue; //skips the loop
+    }
+    print(val[i]);
+  }
+
+  //break
+  for (int i = 0; i < val.length; i++) {
+    if (i == 1 || i == 2 || i == 3) {
+      break; //throws you out of the loop
+    }
+    print(val[i]);
+  }
 }
