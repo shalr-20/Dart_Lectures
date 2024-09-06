3d0b93d41ee6bb5c7185cf053aa7ce5fbf23b581
shalr-20 <shalini.roy2035@gmail.com>
2024-09-06 22:45:43 +0530

    classes

--- classes.dart
+++ classes.dart
@@ -1,11 +1,19 @@
 void main() {
-  print(Cookie());
+  // Cookie c = Cookie(); //Storing instance of the Cookie class in a variable 'c'
+  final c = Cookie();
+  //print('${Cookie().size} cm');
+  // c.baking();
+  // print(c.isCooling());
+  print(c.shape = 'Rectangle');
+  print(c.size = 6.2);
 }
 
 class Cookie {
+  //varibales
   String shape = 'Circle';
   double size = 14.2; //cm
 
+  //methods
   void baking() {
     print('Baking in progress');
   }
