a7b388926bdf54c664107950f097672965ac2836
shalr-20 <shalini.roy2035@gmail.com>
2024-09-14 17:00:15 +0530

    encapsulation

---
+++ encapsulation.dart
@@ -0,0 +1,7 @@
+void main() {}                    //function is public
+
+class _Person {                  //class is private
+  String _name = 'Hello';        //variabe is private
+
+  void _getName() {}             //function is private
+}
