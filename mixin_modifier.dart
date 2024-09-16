643c658e6b601389091bdcaa882a39d066605740
shalr-20 <shalini.roy2035@gmail.com>
2024-09-16 11:37:26 +0530

    mixin_modifier

---
+++ mixin_modifier.dart
@@ -0,0 +1,15 @@
+void main() {
+  Animal4(); 
+}
+
+// sealed class Animal {}
+
+// final class Animal1 {}
+
+// base class Animal2 {}
+
+// interface class Animal3 {}
+
+mixin class Animal4 {}
+
+class Human with Animal4{}
